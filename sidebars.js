707e9ea299e811edf9fedf1558f6c9c62e499f18
laboone <win_iox@hotmail.com>
2023-03-18 22:38:36 +0900

    update

--- sidebars.js
+++ sidebars.js
@@ -78,6 +78,10 @@ const sidebars = {
           type: 'doc',
           id: 'plan/cancel-plan',
         },
+        {
+          type: 'doc',
+          id: 'plan/change-email',
+        },
       ]
     },
     {
