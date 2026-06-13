9a23f242a600d6877212cd09dc84354673f25e11
laboone <win_iox@hotmail.com>
2026-06-13 15:14:39 +0900

    upddate

--- static/root-redirect.js
+++ static/root-redirect.js
@@ -1,3 +1,16 @@
+function removeFromLocaleSwitchParam(pathname) {
+  var searchParams = new URLSearchParams(window.location.search);
+  if (searchParams.get('fromLocaleSwitch') !== '1') {
+    return false;
+  }
+
+  searchParams.delete('fromLocaleSwitch');
+  var newQuery = searchParams.toString();
+  var newUrl = pathname + (newQuery ? '?' + newQuery : '') + window.location.hash;
+  window.history.replaceState({}, '', newUrl);
+  return true;
+}
+
 (function () {
   if (typeof window === 'undefined') {
     return;
@@ -5,15 +18,11 @@
   var pathname = window.location.pathname;
 
   if (pathname !== '/' && pathname !== '') {
+    removeFromLocaleSwitchParam(pathname);
     return;
   }
 
-  var searchParams = new URLSearchParams(window.location.search);
-  if (searchParams.get('fromLocaleSwitch') === '1') {
-    searchParams.delete('fromLocaleSwitch');
-    var newQuery = searchParams.toString();
-    var newUrl = pathname + (newQuery ? '?' + newQuery : '') + window.location.hash;
-    window.history.replaceState({}, '', newUrl);
+  if (removeFromLocaleSwitchParam(pathname)) {
     return;
   }
 
